8a56281014639223b77e80611126db0acc4e50c1
Zottola <pablozottola66@gmail.com>
2024-03-26 21:47:20 -0300

    fixApp

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import { ProductProvider } from "./src/providers/ProductProvider";
 import { BottomTabs } from "./src/screen/navigations/BottomTabs";
 import { FormProvider } from "./src/providers/FormProvider";
 import { StackNavigators } from "./src/screen/navigations/StackNavigators";
+import { StatusBar } from "react-native";
 
 export default function App() {
   return (
     <NavigationContainer>
       <FormProvider>
         <ProductProvider>
+          <StatusBar />
           <StackNavigators>
             <BottomTabs />
           </StackNavigators>
