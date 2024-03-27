6fb6512235f33d2da030ea0b0507e9ece41621e6
gabogarcia1 <gabegarcia916@gmail.com>
2024-03-26 21:42:20 -0300

    añadir custom productsrenderItems

--- src/screen/CategoriesScreen.jsx
+++ src/screen/CategoriesScreen.jsx
@@ -1,94 +1,74 @@
-import { useContext, useEffect, useState } from 'react';
-import { StyleSheet, Text, View,FlatList, TouchableOpacity, Image, SafeAreaView, ActivityIndicator} from 'react-native';
-import { productsStyles } from '../styles/productsStyles';
-import { globalStyles } from '../styles/globalStyles';
-import { ProductContext } from '../contexts/ProductContext';
-import { useNavigation } from '@react-navigation/native';
+import { useContext, useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  ActivityIndicator,
+} from "react-native";
+import { productsStyles } from "../styles/productsStyles";
+import { globalStyles } from "../styles/globalStyles";
+import { ProductContext } from "../contexts/ProductContext";
+import { useNavigation } from "@react-navigation/native";
+import { renderProductsItem } from "./components/products/CustomProductsRenderItem";
 
-
-export default Categories=({route}) => {
-    const { item } = route.params;
+export default Categories = ({ route }) => {
+  const { item } = route.params;
   const navigation = useNavigation();
-  const {state, getProducts,getCategories,toggleFavorite} = useContext(ProductContext); 
+  const { state, getProducts, getCategories, toggleFavorite } =
+    useContext(ProductContext);
 
-      useEffect(() => {
-          init();
-      },[]);
+  useEffect(() => {
+    init();
+  }, []);
 
-      const init = async () => {
-        getProducts();
-        getCategories()
-    }
+  const init = async () => {
+    getProducts();
+    getCategories();
+  };
 
-
-  const renderProductsItem = ({item}) => {
-    return (
-      <TouchableOpacity
-        style={productsStyles.cardProduct}
-        onPress={()=>navigation.navigate("ProductItem", { item: item })}>
-        <View style={productsStyles.cardProductHeader}>
-          <TouchableOpacity onPress={() => toggleFavorite(item.id)}>
-            <Image
-                    style={{ height: 25, width: 25}}
-                    source={state.favorites.includes(item.id) ? require('../../assets/favorite-black.png') : require('../../assets/favorite.png') }
-            />
-          </TouchableOpacity>
-
-
-        </View>
-        <Image style={productsStyles.productImage} source={{ uri: item.image }} />
-        <View style={productsStyles.cardProductFooter}>
-          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-            <Text numberOfLines={2} style={productsStyles.titleProduct}>{item.title}</Text>
-            <Text numberOfLines={3} style={productsStyles.descriptionProduct}>{item.description}</Text>
-            <Text style={productsStyles.priceProduct}>$ {item.price.toFixed(2)}</Text>
-          </View>
-        </View>
-      </TouchableOpacity>
-    )
-  }
-
-  const productsByCategorie =(categorie)=>{
-    const products= state.products
-    return products.filter(producto => producto.category===categorie)
-  }
+  const productsByCategorie = (categorie) => {
+    const products = state.products;
+    return products.filter((producto) => producto.category === categorie);
+  };
   return (
-    <View  style={{ flex: 1}}>
-     {state.isLoading ? (
+    <View style={{ flex: 1 }}>
+      {state.isLoading ? (
         <ActivityIndicator size="large" color="#000000" />
       ) : (
-      <View style={productsStyles.productsContainer}>
-            <View style={productsStyles.headerContainer}>
-              <TouchableOpacity>
-                <Image 
-                            style={{height: 35, width: 35}}
-                            source={require('../../assets/arrow.png')}
-                          />
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Image
-                            style={{height: 22, width: 22}}
-                            source={require('../../assets/search.png')}
-                          />
-              </TouchableOpacity>
-            </View>
-            <View>
-              <Text style={globalStyles.titleStyle}>Productos de {item} </Text>
-                <FlatList
-                  contentContainerStyle={productsStyles.listProductsContainer}
-                  data={productsByCategorie(item)}
-                  horizontal={false}
-                  numColumns={2}
-                  keyExtractor={item => {
-                    return item.id
-                  }}
-                  renderItem={renderProductsItem}
-                /> 
-            </View>
-      </View> 
-     )}
+        <View style={productsStyles.productsContainer}>
+          <View style={productsStyles.headerContainer}>
+            <TouchableOpacity>
+              <Image
+                style={{ height: 35, width: 35 }}
+                source={require("../../assets/arrow.png")}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                style={{ height: 22, width: 22 }}
+                source={require("../../assets/search.png")}
+              />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text style={globalStyles.titleStyle}>Productos de {item} </Text>
+            <FlatList
+              contentContainerStyle={productsStyles.listProductsContainer}
+              data={productsByCategorie(item)}
+              horizontal={false}
+              numColumns={2}
+              keyExtractor={(item) => {
+                return item.id;
+              }}
+              renderItem={renderProductsItem}
+            />
+          </View>
+        </View>
+      )}
     </View>
-
-
   );
-}
+};
--- src/screen/ProductsScreen.jsx
+++ src/screen/ProductsScreen.jsx
@@ -1,90 +1,67 @@
-import { useContext, useEffect, useState } from 'react';
-import { StyleSheet, Text, View,FlatList, TouchableOpacity, Image, SafeAreaView, ActivityIndicator} from 'react-native';
-import { productsStyles } from '../styles/productsStyles';
-import { globalStyles } from '../styles/globalStyles';
-import { ProductContext } from '../contexts/ProductContext';
-import { useNavigation } from '@react-navigation/native';
-
+import { useContext, useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  ActivityIndicator,
+} from "react-native";
+import { productsStyles } from "../styles/productsStyles";
+import { globalStyles } from "../styles/globalStyles";
+import { ProductContext } from "../contexts/ProductContext";
+import { useNavigation } from "@react-navigation/native";
+import { renderProductsItem } from "./components/products/CustomProductsRenderItem";
 
 export default function Products() {
-
   const navigation = useNavigation();
-  const {state, getProducts,toggleFavorite} = useContext(ProductContext); 
+  const { state, getProducts, toggleFavorite } = useContext(ProductContext);
 
-      useEffect(() => {
-          init();
-      },[]);
+  useEffect(() => {
+    init();
+  }, []);
 
-      const init = async () => {
-        getProducts();
-    }
-
-
-  const renderProductsItem = ({item}) => {
-    return (
-      <TouchableOpacity
-        style={productsStyles.cardProduct}
-        onPress={()=>navigation.navigate("ProductItem", { item: item })}>
-        <View style={productsStyles.cardProductHeader}>
-          <TouchableOpacity onPress={() => toggleFavorite(item.id)}>
-            <Image
-                    style={{ height: 25, width: 25}}
-                    source={state.favorites.includes(item.id) ? require('../../assets/favorite-black.png') : require('../../assets/favorite.png') }
-            />
-          </TouchableOpacity>
-
-
-        </View>
-        <Image style={productsStyles.productImage} source={{ uri: item.image }} />
-        <View style={productsStyles.cardProductFooter}>
-          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-            <Text numberOfLines={2} style={productsStyles.titleProduct}>{item.title}</Text>
-            <Text numberOfLines={3} style={productsStyles.descriptionProduct}>{item.description}</Text>
-            <Text style={productsStyles.priceProduct}>$ {item.price.toFixed(2)}</Text>
-          </View>
-        </View>
-      </TouchableOpacity>
-    )
-  }
-
+  const init = async () => {
+    getProducts();
+  };
 
   return (
-    <View  style={{ flex: 1}}>
-     {state.isLoading ? (
+    <View style={{ flex: 1 }}>
+      {state.isLoading ? (
         <ActivityIndicator size="large" color="#000000" />
       ) : (
-      <View style={productsStyles.productsContainer}>
-            <View style={productsStyles.headerContainer}>
-              <TouchableOpacity>
-                <Image 
-                            style={{height: 35, width: 35}}
-                            source={require('../../assets/arrow.png')}
-                          />
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Image
-                            style={{height: 22, width: 22}}
-                            source={require('../../assets/search.png')}
-                          />
-              </TouchableOpacity>
-            </View>
-            <View>
-              <Text style={globalStyles.titleStyle}>Productos</Text>
-                <FlatList
-                  contentContainerStyle={productsStyles.listProductsContainer}
-                  data={state.products}
-                  horizontal={false}
-                  numColumns={2}
-                  keyExtractor={item => {
-                    return item.id
-                  }}
-                  renderItem={renderProductsItem}
-                /> 
-            </View>
-      </View> 
-     )}
+        <View style={productsStyles.productsContainer}>
+          <View style={productsStyles.headerContainer}>
+            <TouchableOpacity>
+              <Image
+                style={{ height: 35, width: 35 }}
+                source={require("../../assets/arrow.png")}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                style={{ height: 22, width: 22 }}
+                source={require("../../assets/search.png")}
+              />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text style={globalStyles.titleStyle}>Productos</Text>
+            <FlatList
+              contentContainerStyle={productsStyles.listProductsContainer}
+              data={state.products}
+              horizontal={false}
+              numColumns={2}
+              keyExtractor={(item) => {
+                return item.id;
+              }}
+              renderItem={renderProductsItem}
+            />
+          </View>
+        </View>
+      )}
     </View>
-
-
   );
 }
