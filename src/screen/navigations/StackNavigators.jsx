a63492cc8b5d145835066dce7e1df1c8247dfa44
FlorRoth <roth.florencia99@gmail.com>
2024-04-11 18:10:49 -0300

    add screen payment

--- src/screen/navigations/StackNavigators.jsx
+++ src/screen/navigations/StackNavigators.jsx
@@ -6,8 +6,9 @@ import SignUpScreen from "../SignUpScreen.jsx";
 import SuccessfulScreen from "../SuccessfulScreen.jsx";
 import Categories from "../CategoriesScreen.jsx";
 import Products from "../ProductsScreen.jsx";
-import ProductItem from "../ProductItemScreen.jsx";
-import Cart from "../CartScreen.jsx";
+import ProductItemScreen from "../ProductItemScreen.jsx"
+import PaymentScreen from "../PaymentScreen.jsx"
+import Cart from "../CartScreen.jsx"
 
 const Stack = createStackNavigator();
 
@@ -47,71 +48,59 @@ export const StackNavigators = () => {
             elevation: 0,
           },
         }}
-      />
-      <Stack.Screen
-        name="SuccessfulScreen"
-        options={{
-          title: "",
-          headerStyle: {
-            backgroundColor: "#ffff",
-            elevation: 0,
-          },
-        }}
-        component={SuccessfulScreen}
-      />
-      <Stack.Screen
-        options={{
-          title: "",
-          headerStyle: {
-            backgroundColor: "#ffff",
-            elevation: 0,
-          },
-        }}
-        name="ProductItem"
-        component={ProductItem}
-      />
-      <Stack.Screen
-        options={{
-          title: "",
-          headerStyle: {
-            backgroundColor: "#ffff",
-            elevation: 0,
-          },
-        }}
-        name="Products"
-        component={Products}
-      />
-      <Stack.Screen
-        options={{
-          title: "",
-          headerStyle: {
-            backgroundColor: "#ffff",
-            elevation: 0,
-          },
-        }}
-        name="PageNotFound"
-        component={PageNotFound}
-      />
-      <Stack.Screen
-        name="Categories"
-        component={Categories}
-        options={{
-          title: "",
-          headerStyle: {
-            elevation: 0,
-          },
-        }}
-      />
-      <Stack.Screen
-        name="Cart"
-        component={Cart}
-        options={{
-          title: "",
-          headerStyle: {
-            elevation: 0,
-          },
-        }}
-      />
+      /> 
+      <Stack.Screen options={{
+        title: "",
+        headerStyle: {
+          backgroundColor: "#ffff",
+          elevation: 0,
+        },
+      }} name="SuccessfulScreen" component={SuccessfulScreen} /> 
+      <Stack.Screen options={{
+        title: "",
+        headerStyle: {
+          backgroundColor: "#ffff",
+          elevation: 0,
+        },
+      }} name="Products" component={Products} />
+
+
+      <Stack.Screen options={{
+        title: "",
+        headerStyle: {
+          backgroundColor: "#ffff",
+          elevation: 0,
+        },
+      }} name="ProductItem" component={ProductItemScreen} />
+
+      <Stack.Screen options={{
+        title: "",
+        headerStyle: {
+          backgroundColor: "#ffff",
+          elevation: 0,
+        },
+      }} name="PageNotFound" component={PageNotFound} />
+      <Stack.Screen name="Categories" component={Categories}
+      options={{
+        title: "",
+        headerStyle: {
+          elevation: 0,
+        },
+      }} />
+      <Stack.Screen name="Cart" component={Cart}
+      options={{
+        title: "",
+        headerStyle: {
+          elevation: 0,
+        },
+      }} /> 
+            <Stack.Screen name="Payment" component={PaymentScreen}
+      options={{
+        title: "",
+        headerStyle: {
+          elevation: 0,
+        },
+      }} />
     </Stack.Navigator>
   );
 };
